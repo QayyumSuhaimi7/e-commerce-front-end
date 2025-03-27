e47ead93003bec2ca64693c15043e69c2a432bbc
Qayyum Suhaimi <qayyumsuhaimi10@gmail.com>
2025-03-27 16:14:40 +0800

    Create Nav,Header, Footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<h1>Product Component</h1>} />
+          <Route path="/add" element={<h1>Add Component</h1>} />
+          <Route path="/update" element={<h1>Update Component</h1>} />
+          <Route path="/logout" element={<h1>Logout Component</h1>} />
+          <Route path="/profile" element={<h1>Profile Component</h1>} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
